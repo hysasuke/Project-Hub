50a7004bc125322a9e825032b104be472e8dcbfc
MentalArea <113bennylee@gmail.com>
2023-04-19 16:12:42 -0400

    ✨ feat: Frontend Update

---
+++ public/_next/static/W_wayyeOqmWK0uNQyUapX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/874-5ba259c7667b6eac.js","static/css/33d7a9e1a86eb6f9.css","static/chunks/pages/index-37be23cbdb1e4ce2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- public/_next/static/xV8OOk9KzMjScxio8_dVF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/874-5ba259c7667b6eac.js","static/css/33d7a9e1a86eb6f9.css","static/chunks/pages/index-a4190ea650614a1a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
