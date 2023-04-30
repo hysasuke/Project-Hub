d9dcbac4002018a15fbe586b2779bfd21cce0d90
Wenhao Li <113bennylee@gmail.com>
2023-04-30 14:11:27 -0400

    Revert "v0.1.0 Client Update"

---
+++ public/_next/static/F_G2dpIYs7z2QI0EuND17/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/294-f85a82114355d920.js","static/css/33d7a9e1a86eb6f9.css","static/chunks/pages/index-c653221e4fc78e4b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
