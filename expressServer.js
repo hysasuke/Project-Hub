40d538e55e1a26ca8aae0a1bbbbea9d2dcaa8d78
MentalArea <113bennylee@gmail.com>
2023-04-30 13:27:11 -0400

    🐞 fix: Windows Adapt

--- expressServer.js
+++ expressServer.js
@@ -17,7 +17,11 @@ const {
   reorderGroupItems,
   reorderGroups
 } = require("./Controllers/group-controller");
-const { selectFile } = require("./Controllers/system-controller");
+const {
+  selectFile,
+  shutdown,
+  restart
+} = require("./Controllers/system-controller");
 const { handleGroupItem } = require("./Controllers/group-item-controller");
 const {
   getHeaderComponents,
@@ -89,6 +93,14 @@ function startExpressServer() {
 
   expressApp.get("/requestFileSelection", (req, res) => {
     selectFile(req, res);
+  });
+
+  expressApp.get("/system/shutdown", (req, res) => {
+    shutdown();
+  });
+
+  expressApp.get("/system/restart", (req, res) => {
+    restart();
   });
 
   expressApp.post("/system/volume", (req, res) => {
