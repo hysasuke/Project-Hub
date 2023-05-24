51493387ffe217aa86558b1fc13628233c6b1d91
MentalArea <113bennylee@gmail.com>
2023-05-24 17:34:31 -0400

    🐞 fix: Client Bug Fix

--- public/_next/static/cj7FhKqM4nBcC5tZ-GGYp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/260-4a1bff7786900790.js","static/css/951219c27de3f685.css","static/chunks/pages/index-04521f5922613c2d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
